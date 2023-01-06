c36805569683697fb90d7e555caba249a7bb35e7
Devin Humphreys <devinhumphreys@hotmail.com>
2023-01-05 22:53:21 -0500

    JSX Beginner App

---
+++ jsx/src/index.js
@@ -0,0 +1,14 @@
+// 1) Import React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './components/App';
+
+// 2) Get a reference to the div with an id of root
+const element = document.getElementById('root');
+
+// 3) Tell React to take control of the div element
+const root = ReactDOM.createRoot(element);
+
+// 5) Show the component on the screen
+root.render(<App/>)
