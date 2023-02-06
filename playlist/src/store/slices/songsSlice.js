8b01ef29e59d7bc461e57e5c96bc30f760689a70
Devin Humphreys <devinhumphreys@hotmail.com>
2023-02-06 17:36:04 -0500

    Add playlist app w/ redux

---
+++ playlist/src/store/slices/songsSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { reset } from "../actions";
+
+const songsSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      // `state` is ONLY the piece of state managed by this reducer
+      state.push(action.payload);
+    },
+    removeSong(state, action) {
+      state.splice(state.indexOf(action.payload), 1);
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(reset, (state, action) => {
+      return [];
+    });
+  },
+});
+
+export const { addSong, removeSong } = songsSlice.actions;
+export const songsReducer = songsSlice.reducer;
